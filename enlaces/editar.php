ec77366f7ae7f2b2c0051f55908678a74262605c
JOSE FELIX RIVAS <juniorrivas185@hotmail.com>
2018-08-27 18:50:12 -0400

    Se ha configurado correctamente la edicion de menus - Lo que conlleva a editar un campo de los submenus -, falta los submenus y articulos

--- enlaces/editar.php
+++ enlaces/editar.php
@@ -7,7 +7,8 @@
 			$m_m = explode("-", $_POST['m_m']);
 			$m_titulo = $_POST['m_titulo'];
 			$m_posicion = $_POST['m_posicion'];
-			$m_url1 = "../../m/".$m_titulo."/";
+			$m_url = "m/".$m_titulo."/";
+			$m_url1 = "../../".$m_url;
 			$m_url2= "../../m/".$m_titulo."/p/";
 			$m_url3 = $m_url2."datos_ind.php";
 
@@ -17,15 +18,29 @@
 			if(!rename($dir1, $m_url1)){
 				echo "Fallo al editar (Renombrado) | <span>CPW Online</span>";
 			}else{
-				$con = $mysqli->query("UPDATE menus SET m_titulo = '".$m_titulo."', m_posicion = '".$m_posicion."', m_url = '".$m_url1."' WHERE menus.m_id = '".$m_m[0]."'");
+				//Modificado en el menú
+					$con = $mysqli->query("UPDATE menus SET m_titulo = '".$m_titulo."', m_posicion = '".$m_posicion."', m_url = '".$m_url."' WHERE menus.m_id = '".$m_m[0]."'");
 				if($con){
 					//Creado del archivo de información
 						$FP = FOPEN($m_url3, "w");
 						FPUTS($FP, $m_titulo);
 						FCLOSE($FP);
-					echo 'Edici&oacute;n realizada correctamente.';
+					//Modificado en los submenús
+						//Consultamos los submenus
+							$con_t =  $mysqli->query("SELECT s_titulo FROM menus WHERE menus.m_id = '".$m_m[0]."'");
+						//Vemos si hay 
+							if($con_t !== 0){
+								//Si hay 
+									$con2 = $mysqli->query("UPDATE submenus SET s_menu='".$m_titulo."' WHERE submenus.s_menu = '".$m_m[1]."'");
+									if($con2)
+										echo 'Edici&oacute;n realizada correctamente.';
+									else
+										echo "Fallo al editar (Registro:Submen&uacute;) | <span>CPW Online</span>";
+							}else{
+								echo 'Edici&oacute;n realizada correctamente.';
+							}
 				}else
-					echo "Fallo al editar (Registro) | <span>CPW Online</span>";
+					echo "Fallo al editar (Registro:Men&uacute;) | <span>CPW Online</span>";
 				break;
 			}
 
